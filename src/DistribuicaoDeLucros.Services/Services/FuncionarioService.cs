7ebece4f8c82342a1c78d2c57dd87aedbc098d23
Michel Candido <michelmarcoscandido@gmail.com>
2021-12-28 21:36:46 -0300

    Validação ao inserir o funcionário

--- src/DistribuicaoDeLucros.Services/Services/FuncionarioService.cs
+++ src/DistribuicaoDeLucros.Services/Services/FuncionarioService.cs
@@ -2,6 +2,7 @@ using System;
 using DistribuicaoDeLucros.Domain.Entities;
 using DistribuicaoDeLucros.Domain.Interfaces.Repositories;
 using DistribuicaoDeLucros.Domain.Interfaces.Services;
+using FluentValidation;
 using SE.EntityFrameworkCore.UnitOfWork;
 
 namespace DistribuicaoDeLucros.Services.Services
@@ -10,17 +11,27 @@ namespace DistribuicaoDeLucros.Services.Services
     {
         private readonly IFuncionarioRepository funcionarioRepository;
         private readonly IUnitOfWork unitOfWork;
+        private readonly AbstractValidator<Funcionario> funcionarioValidator;
         private readonly IAreaRepository areaRepository;
 
-        public FuncionarioService(IFuncionarioRepository funcionarioRepository, IUnitOfWork unitOfWork)
+        public FuncionarioService(
+            IFuncionarioRepository funcionarioRepository, 
+            IUnitOfWork unitOfWork, 
+            AbstractValidator<Funcionario> funcionarioValidator)
         {
             this.funcionarioRepository = funcionarioRepository;
             this.unitOfWork = unitOfWork;
+            this.funcionarioValidator = funcionarioValidator;
         }
 
         public async Task ArmazenarFuncionariosAsync(List<Funcionario> funcionarios)
         {
-            await funcionarioRepository.InsertAsync(funcionarios);
+
+            funcionarios.ForEach(async funcionario => {
+                if(funcionarioValidator.Validate(funcionario).IsValid) {
+                    await funcionarioRepository.InsertAsync(funcionario);
+                }
+            });
             await unitOfWork.SaveChangesAsync();
         }
     }
